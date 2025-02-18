500429ca61de83cd7d21543588065aaa7c23bf6f
shrimp <nhanshrimp@gmail.com>
2025-02-18 19:03:47 +0700

    firstTimeToEverCommitAGithubChange

---
+++ main.py
@@ -0,0 +1,161 @@
+import pygame 
+import time
+import random
+
+# Khoi tao cac thu 
+
+pygame.init()
+surface = pygame.display.set_mode((1000, 1000))
+bg = pygame.image.load('migu.png').convert(24)
+myfont = pygame.font.SysFont("Comic Sans MS", 30)
+label = myfont.render("Your score UwU: ", 1, (255, 255, 0))
+dangThuc = pygame.image.load('dangThuc.png').convert()
+
+playerScore = 0
+currentX = 250
+currentY = 0
+currentRotation = 0
+maxI = [450, 300]
+maxZ = [350, 400]
+maxO = [400]
+
+grid = [[0 for x in range(10)] for y in range(20)]
+
+pieceI = [
+            [
+                [1],
+                [1],
+                [1],
+                [1],
+            ], 
+            
+            [
+                [1, 1, 1, 1]
+            ],
+        ]
+pieceZ = [ 
+            [
+                [1, 1, 0],
+                [0, 1, 1],
+            ], 
+
+            [
+                [0, 1],
+                [1, 1],
+                [1, 0]
+            ],
+        ]
+pieceO = [
+            [
+                [1, 1],
+                [1, 1]
+            ],
+
+        ]
+
+##############################################################################
+class __init__:
+    
+    def drawGridAfter(surface):
+        for i in range(20):
+            for j in range(10):
+                if grid[i][j] == 1:
+                    pygame.draw.rect(surface, (0, 0, 255), [j * 50, i * 50, 50, 50], 0)
+
+    def drawGrid(surface):
+        blockSize = 50
+        for x in range(0, 500, blockSize):
+            for y in range(0, 1000, blockSize):
+                rect = pygame.Rect(x, y, blockSize, blockSize)
+                pygame.draw.rect(surface, (255, 255, 255), rect, 1)
+    
+    def drawPiece(surface, piece, x, y, rotation):
+        __init__.drawGrid(surface)
+        for i in range(len(piece[rotation])):
+            for j in range(len(piece[rotation][i])):
+                if piece[rotation][i][j] == 1:
+                    pygame.draw.rect(surface, (0, 0, 255), [x + j * 50, y + i * 50, 50, 50], 0)
+    
+    def drawCurrentPiece(surface):
+        pygame.draw.rect(surface, (0, 0, 255), [700, 50, 300, 475], 2)
+##############################################################################
+
+class scoreUpdate:
+    def updateScore(score):
+        score += 1
+        return score
+
+    def printScore(surface, myfont, score):
+        showScore = myfont.render(str(score), 1, (255, 255, 0))
+        surface.blit(showScore, (975, 5))
+
+##############################################################################
+
+def pickPiece():
+    return random.choice([pieceI, pieceZ, pieceO])
+
+def processPiece(surface, piece, rotation, currentX, currentY):
+    surface.blit(bg, (0, 0))
+    surface.blit(label, (700, 5))
+    __init__.drawGrid(surface)
+    __init__.drawGridAfter(surface)
+    __init__.drawCurrentPiece(surface)
+    __init__.drawPiece(surface, piece, currentX, currentY, rotation)
+    __init__.drawPiece(surface, currentPiece, 800, 100, currentRotation)
+
+def checkLimitX(piece, rotation, currentX):
+    if (piece == pieceI and currentX > maxI[rotation]): 
+        currentX = maxI[rotation]
+    if (piece == pieceZ and currentX > maxZ[rotation]):
+        currentX = maxZ[rotation]
+    if (currentX < 0):
+        currentX = 0
+    return currentX
+
+def checkLimitY(currentPiece, currentRotation, currentX, currentY):
+    if (currentY > 1000 - len(currentPiece[currentRotation]) * 50):
+        currentY = 0
+        currentPiece = pickPiece()
+        currentX = 250
+        currentY = 0
+        currentRotation = 0
+    return currentPiece, currentRotation, currentX, currentY
+
+def changeRotation(piece, rotation, currentX):
+    if (rotation == len(piece) - 1):
+        rotation = 0
+    else:
+        rotation += 1
+    currentX = checkLimitX(piece, rotation, currentX)
+    return currentX, rotation
+
+# Main loop
+currentPiece = pickPiece()
+while True:
+    # pygame.display.update() 
+    currentPiece, currentRotation, currentX, currentY = checkLimitY(currentPiece, currentRotation, currentX, currentY)
+    processPiece(surface, currentPiece, currentRotation, currentX, currentY)
+    # changeRotation(pieceZ, currentRotation) 
+    for event in pygame.event.get(): 
+        if (event.type == pygame.KEYDOWN):
+            if (event.key == pygame.K_LEFT):
+                currentX -= 50
+                currentX = checkLimitX(currentPiece, currentRotation, currentX)
+                processPiece(surface, currentPiece, currentRotation, currentX, currentY)
+            if (event.key == pygame.K_RIGHT):
+                currentX += 50
+                currentX = checkLimitX(currentPiece, currentRotation, currentX)
+                processPiece(surface, currentPiece, currentRotation, currentX, currentY)
+            if (event.key == pygame.K_UP):
+                currentX, currentRotation = changeRotation( currentPiece, currentRotation, currentX)
+                processPiece(surface,  currentPiece, currentRotation, currentX, currentY)
+            # if (event.key == pygame.K_DOWN):
+            #     while (pygame.key.get_pressed()[pygame.K_DOWN]):
+            #         currentY += 50
+            #     processPiece(surface,  currentPiece, currentRotation, currentX, currentY)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+    currentY += 50
+    pygame.display.flip()
+    time.sleep(0.2)
